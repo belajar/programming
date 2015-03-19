ce4b7beb55a8ce0ae7e807490fa7dcb9fc37190d
Kamal Mustafa <kamal@randomquran.github.io>
2015-03-19 08:33:46 +0800

    set active on current view

--- javascripts/nav.js
+++ javascripts/nav.js
@@ -4,6 +4,7 @@
     viewport_view = document.getElementById('viewport-view');
     if (!current_view) {
         viewport_view.innerHTML = document.getElementById('viewport-home').innerHTML;
+        current_view = nav_elm.children[0];
     }
     nav_elm.addEventListener('click', function (e) {
         var hash, viewport;
@@ -11,7 +12,9 @@
             hash = e.target.hash.split('#')[1];
             viewport = document.getElementById('viewport-' + hash);
             viewport_view.innerHTML = viewport.innerHTML;
-            current_view = hash;
+            e.target.parentElement.className = 'active';
+            current_view.className = '';
+            current_view = e.target.parentElement;
         }
     });
 }());
